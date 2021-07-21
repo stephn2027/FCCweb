a5fa4c961df6ca986f3a0d6e260ecd5a9e379c78
kor <stephn2027@gmail.com>
2021-07-21 12:53:03 +0900

    completed footer section

--- porfolio/porfolio.js
+++ porfolio/porfolio.js
@@ -207,28 +207,5 @@ document.querySelector('#close')
 });
 
   
+  //footer icon animation
   
-
-
-
-// CONTACT FORM
-// $('#contact-form').submit(function(e) {
-//   e.preventDefault();
-
-//   $.ajax({
-//     url: 'https://formspree.io/stephn2027@gmail.com',
-//     method: 'POST',
-//     data: { message: $('form').serialize() },
-//     dataType: 'json'
-//   }).done(function(response) {
-//     $('#success').addClass('expand');
-//     $('#contact-form')
-//       .find('input[type=text], input[type=email], textarea')
-//       .val('');
-//   });
-// });
-
-// $('#close').click(function() {
-//   $('#success').removeClass('expand');
-// });
-

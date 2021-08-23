924e6c790f72049721c24a7dcd2801cf79cf3a89
Stephen James Uy <69063695+stephn2027@users.noreply.github.com>
2021-08-24 06:56:15 +0900

    addedCsstoIndex.js

---
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render(){
+        return (
+        <React.Fragment>
+        <p>fish</p>
+
+        
+        <form className="store-selector">
+            <h2>Please enter a store</h2>
+            <input type="text" required placeholder="Store Name" />
+            <button type="submit">Visit Store ➢</button>
+        </form>
+        </React.Fragment>
+        )
+        
+    }
+}
+
+export default StorePicker;

e3a646e462dd08b79afcc3232bd88a7b75b53bcf
Stephen James Uy <69063695+stephn2027@users.noreply.github.com>
2021-08-26 13:12:56 +0900

    newcomponents/catchoftheday

---
+++ React/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = (event) => {
+    event.preventDefault();
+    const fish = {
+      name: this.nameRef.current.value,
+      price: parseFloat(this.priceRef.current.value),
+      status: this.statusRef.current.value,
+      desc: this.descRef.current.value,
+      image: this.imageRef.current.value,
+    };
+    this.props.addFish(fish);
+    
+    //refresh the form
+    event.currentTarget.reset();
+    event.currentTarget.focus();
+  };
+
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" type="text" ref={this.nameRef} placeholder="Name" />
+        <input
+          name="price"
+          type="text"
+          ref={this.priceRef}
+          placeholder="Price"
+        />
+        <select name="status" ref={this.statusRef}>
+          <option value="available">Fresh</option>
+          <option value="unavailable">Sold Out</option>
+        </select>
+        <textarea
+          name="desc"
+          type="text"
+          ref={this.descRef}
+          placeholder="Desc"
+        ></textarea>
+        <input
+          name="image"
+          type="text"
+          ref={this.imageRef}
+          placeholder="Image"
+        />
+        <button type="submit">+ Submit</button>
+      </form>
+    );
+  }
+}
+
+export default AddFishForm;
--- React/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
+++ React/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
@@ -1,16 +1,39 @@
 import React from 'react';
+import {getFunName} from "../helpers";
 
 class StorePicker extends React.Component {
+
+    // constructor(){
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+     myInput = React.createRef();
+     
+//"this" cannot be accessed inside a custom method because it has no bindings to the React Component so "this" keyword will equal to undefined;
+//use constructor(for 2 or less methods) or declare a property that is set to have an arrow funtion to access "this" in a custom method.
+//properties will be bounded to an instance rather than nothing when using a custom method 
+    goToStore = (event)=>{
+        //prevent submit default
+        event.preventDefault();
+        
+        //getting the text from input
+    //    const textInput = this.myInput.current.value;
+       const storeName = this.myInput.current.value;
+        //change the page to store/user input
+        //push state allows us to change a state without refreshing the page.
+        console.log(this.props.history.push(`/store/${storeName}`));
+    };
+
     render(){
         return (
         <React.Fragment>
-        <p>fish</p>
+        
 
         
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
             <h2>Please enter a store</h2>
-            <input type="text" required placeholder="Store Name" />
-            <button type="submit">Visit Store ➢</button>
+            <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()}/>
+            <button type="submit" >Visit Store ➢</button>
         </form>
         </React.Fragment>
         )

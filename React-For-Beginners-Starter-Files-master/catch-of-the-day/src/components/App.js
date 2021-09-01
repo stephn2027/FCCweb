978f22b91925e9fa6878e82ace05247f95fee7ab
Stephen James Uy <69063695+stephn2027@users.noreply.github.com>
2021-09-01 19:13:10 +0900

    newFileLoginAuth

--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
@@ -1,27 +1,51 @@
-import React from 'react';
-import Header from './Header';
-import Inventory from './Inventory';
-import Order from './Order';
-import sampleFishes from '../sample-fishes';
-import Fish from './Fish';
-import base from '../base';
+import React from "react";
+import PropTypes from "prop-types";
+import Header from "./Header";
+import Inventory from "./Inventory";
+import Order from "./Order";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
+  static propTypes = {
+    match: PropTypes.object,
+  };
+
   state = {
     fishes: {},
     orders: {},
   };
-  componentDidMount(){
-      const { params } = this.props.match
-      this.ref = base.syncState(`${params.storeId}/fishes`,{
-          context: this,
-          state: 'fishes',
-      });
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    //1. reinstate the local storage
+
+    const localStorageRef = localStorage.getItem(params.storeId);
+    console.log(localStorageRef);
+    if (localStorageRef) {
+      this.setState({ orders: JSON.parse(localStorageRef) });
+    }
+    // one sync issue with firebase and localStorage.getItem is,
+    //localStorage will try to get the item but still isnt available since
+    //firebase database takes time to sync and load the data. one quick fix for
+    //this is to set the component to check if date is available before rendering.
+
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes",
+    });
   }
+  //storing updates localy or through local storage.
+  componentDidUpdate() {
+    const { params } = this.props.match;
+    localStorage.setItem(params.storeId, JSON.stringify(this.state.orders));
+  }
+
   //to avoid performance issues by listening for changes in the componentdidmount function evrytime the client changes store,we can use componentwillunmount
- componentWillUnmount(){
-  base.removeBinding(this.ref);
- };
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
   addFish = (fish) => {
     //steps to update a state
     //1. taking a copy of the existing state to avoid mutation(changing the original state can cause bugs and performance issues);
@@ -35,7 +59,28 @@ class App extends React.Component {
     });
   };
 
+  updateFish = (key, updatedFish) => {
+    //1.take a copy of the current state
+    const fishes = { ...this.state.fishes };
+    //2.update the state
+    fishes[key] = updatedFish;
+    //3. set the updated data to state
+    this.setState({ fishes });
+  };
+  deleteFish = (key) => {
+    //1. take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    //2. update the state
+    fishes[key] = null;
+    //3. set the updated state to the current state
+    this.setState({ fishes });
+  };
 
+  removeFromOrder = (key) => {
+    const orders = { ...this.state.orders };
+    delete orders[key];
+    this.setState({ orders });
+  };
 
   loadSampleFishes = (fishes) => {
     this.setState({
@@ -43,13 +88,13 @@ class App extends React.Component {
     });
   };
 
-  addToOrder = (key)=>{
-      //1. copy the state
-      const orders = {...this.state.orders};
-      //2. Add the order or update the number of orders
-      orders[key] = orders[key] + 1 || 1; 
-      //3. update the state object by calling setState
-      this.setState({orders,})
+  addToOrder = (key) => {
+    //1. copy the state
+    const orders = { ...this.state.orders };
+    //2. Add the order or update the number of orders
+    orders[key] = orders[key] + 1 || 1;
+    //3. update the state object by calling setState
+    this.setState({ orders });
   };
 
   render() {
@@ -59,14 +104,28 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map((key) => (
-              <Fish key={key} details={this.state.fishes[key]} index={key} addToOrder={this.addToOrder} />
+              <Fish
+                key={key}
+                details={this.state.fishes[key]}
+                index={key}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} orders={this.state.orders}/>
+
+        <Order
+          fishes={this.state.fishes}
+          orders={this.state.orders}
+          removeFromOrder={this.removeFromOrder}
+        />
+
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
+          deleteFish={this.deleteFish}
         />
       </div>
     );
--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Inventory.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/Inventory.js
@@ -1,12 +1,45 @@
 import React from "react";
+import firebase from "firebase";
+
+import PropTypes from "prop-types";
 import AddFishForm from "./AddFishForm";
+import EditFishForm from "./EditFishForm";
+import Login from "./Login";
+import { firebaseApp } from "../base";
+
 
 class Inventory extends React.Component {
+    static propTypes = {
+        fishes:PropTypes.object.isRequired,
+        updateFish:PropTypes.func.isRequired,
+        deleteFish:PropTypes.func.isRequired,
+        loadSampleFishes:PropTypes.func.isRequired,
+    };
+    authHandler= async(authData)=>{
+        console.log(authData);
+    };
+
+    authenticate = (provider)=>{
+        const authProvider =new firebase.auth[`${provider}AuthProvider`]();
+        firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler);
+    };
+
     render(){
+        return <Login authenticate={this.authenticate}/>
         return (
            <div className="inventory">
                <h2>Inventory!!!</h2>
+               <Login/>
+               {Object.keys(this.props.fishes).map(key=>
+               <EditFishForm 
+                    key={key} 
+                    index={key}
+                    fish={this.props.fishes[key]}
+                    updateFish={this.props.updateFish}
+                    deleteFish ={this.props.deleteFish}    
+                    />)}
                <AddFishForm addFish={this.props.addFish} />
+               
                <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
            </div>
         );
